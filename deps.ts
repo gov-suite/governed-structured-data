da6b6eebb479445788f878ff24118b9bb5da3a0e
Shahid N. Shah <shahid@shah.org>
2020-09-27 16:21:05 -0400

    feat: add code generation utilities

--- deps.ts
+++ deps.ts
@@ -4,3 +4,4 @@ export * as docopt from "https://denopkg.com/Eyal-Shalev/docopt.js@v1.0.1/src/do
 export * as serializeJS from "https://denopkg.com/shah/javascript-stringify@v2.0.4/src/index.ts";
 export * as serializeJsStringify from "https://denopkg.com/shah/javascript-stringify@v2.0.4/src/stringify.ts";
 export * as serializeJsTypes from "https://denopkg.com/shah/javascript-stringify@v2.0.4/src/types.ts";
+export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.4/mod.ts";
--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
+export * from "./code.ts";
 export * from "./governed-data-controller.ts";
 export * from "./untyped-data-supplier.ts";
 export * from "./untyped-data-typer.ts";
