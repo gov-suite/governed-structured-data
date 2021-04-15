400c5454f21ecb4ccd71307cf18a2792bc221983
geo_vl <geo@citrusinformatics.com>
2021-04-15 17:17:18 +0530

    fix: use temporary docopt for Deno 1.9

--- deps.ts
+++ deps.ts
@@ -2,7 +2,9 @@ export * as encodingTOML from "https://deno.land/std@0.80.0/encoding/toml.ts";
 export * as encodingYAML from "https://deno.land/std@0.80.0/encoding/yaml.ts";
 export * as fs from "https://deno.land/std@0.80.0/fs/mod.ts";
 export * as path from "https://deno.land/std@0.80.0/path/mod.ts";
-export * as docopt from "https://denopkg.com/Eyal-Shalev/docopt.js@v1.0.5/src/docopt.ts";
+//export * as docopt from "https://denopkg.com/Eyal-Shalev/docopt.js@v1.0.6/src/docopt.ts";
+// TODO: when Eyal-Shalev/docopt.js is upgraded, replace this with latest version:
+export * as docopt from "https://denopkg.com/netspective-studios/docopt-TEMP.js/src/docopt.ts";
 export * as versionHelper from "https://denopkg.com/gov-suite/governed-service-helpers@v0.3.9/version.ts";
 export * as serializeJS from "https://denopkg.com/shah/javascript-stringify@v2.0.4/src/index.ts";
 export * as serializeJsStringify from "https://denopkg.com/shah/javascript-stringify@v2.0.4/src/stringify.ts";
