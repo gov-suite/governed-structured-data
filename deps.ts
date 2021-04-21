9b6bf9b2ef1d45c523a9c75cacff04ba6e78789c
geo_vl <geo@citrusinformatics.com>
2021-04-21 15:19:46 +0530

    update: docopt update for deno1.9.0

--- deps.ts
+++ deps.ts
@@ -1,14 +1,14 @@
-export * as encodingTOML from "https://deno.land/std@0.80.0/encoding/toml.ts";
-export * as encodingYAML from "https://deno.land/std@0.80.0/encoding/yaml.ts";
-export * as fs from "https://deno.land/std@0.80.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.80.0/path/mod.ts";
-//export * as docopt from "https://denopkg.com/Eyal-Shalev/docopt.js@v1.0.6/src/docopt.ts";
+export * as encodingTOML from "https://deno.land/std@0.94.0/encoding/toml.ts";
+export * as encodingYAML from "https://deno.land/std@0.94.0/encoding/yaml.ts";
+export * as fs from "https://deno.land/std@0.94.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.94.0/path/mod.ts";
+//export * as docopt from "https://denopkg.com/Eyal-Shalev/docopt.js@v1.0.7/src/docopt.ts";
 // TODO: when Eyal-Shalev/docopt.js is upgraded, replace this with latest version:
 export * as docopt from "https://denopkg.com/netspective-studios/docopt-TEMP.js/src/docopt.ts";
-export * as versionHelper from "https://denopkg.com/gov-suite/governed-service-helpers@v0.3.9/version.ts";
+export * as versionHelper from "https://denopkg.com/gov-suite/governed-service-helpers@v0.3.15/version.ts";
 export * as serializeJS from "https://denopkg.com/shah/javascript-stringify@v2.0.4/src/index.ts";
 export * as serializeJsStringify from "https://denopkg.com/shah/javascript-stringify@v2.0.4/src/stringify.ts";
 export * as serializeJsTypes from "https://denopkg.com/shah/javascript-stringify@v2.0.4/src/types.ts";
 export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.5/mod.ts";
-export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.8.9/mod.ts";
+export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.9.1/mod.ts";
 export * as safety from "https://denopkg.com/shah/ts-safety@v0.3.1/mod.ts";
